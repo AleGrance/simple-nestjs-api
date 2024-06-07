d14d2ff21db6b96afe19150e7b0fcfb5ceb47199
Alejandro Grance <agrance@ODONTOS.LOCAL>
2024-06-07 11:19:51 -0400

    se agrega auth guard para validar las peticiones http con un apikey

--- src/common/auth/auth.guard.ts
+++ src/common/auth/auth.guard.ts
@@ -6,7 +6,7 @@ export class AuthGuard implements CanActivate {
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
-    const apikey = 'R9bVHW4uFm3A0aSI7jQkM955cOIx5VAZ';
+    const apikey = process.env.API_KEY;
     const request = context.switchToHttp().getRequest() as Request;
 
     if (!request.headers['apikey']) {
